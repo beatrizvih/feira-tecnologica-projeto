834360c287a8c35fdbce6edffcf4365dcf9fdef2
alanafiorelini1 <alana.fiorelini@etec.sp.gov.br>
2025-10-16 22:13:35 -0300

    .

--- feira tecnologica site/charada.js
+++ feira tecnologica site/charada.js
@@ -3,8 +3,10 @@ let charada = document.querySelector(".container-charada p");
 let apertou = false;
 
 function apertouRevelar{
-
+    let apertou = true
 }
 function revelarCharada(){
 
 }
+
+apertouRevelar.addEventListener('click', revelarCharada)
