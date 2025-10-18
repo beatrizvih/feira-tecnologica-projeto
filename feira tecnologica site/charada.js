bd75378ad6587a05c67ef89117c54aee2bd599a6
alanafiorelini1 <alana.fiorelini@etec.sp.gov.br>
2025-10-18 17:22:00 -0300

    terminei o js

--- feira tecnologica site/charada.js
+++ feira tecnologica site/charada.js
@@ -1,12 +1,20 @@
 const botaoCharada = document.querySelector(".btn");
-let charada = document.querySelector(".container-charada p");
-let apertou = false;
 
-function apertouRevelar{
-    let apertou = true
-}
+let charadaResposta = document.querySelector(".container-charada p");
+let textoBotao = document.querySelector(".text")
+let respostaVisivel = false;
+
+
 function revelarCharada(){
-
+    if (!respostaVisivel) { //o ! serve para inverter o valor boleano(q era falso)
+        charadaResposta.textContent = "O silêncio se quebrou, findou a minha glória. Não consigo conviver com a porta do quarto aberta. Eu preciso correr, pois fui descoberta! Assinado: Qualquer tipo de violência."
+        textoBotao.innerHTML = "Voltar"
+    } else {
+        charadaResposta.textContent = "Caro leitor, você sabe sobre qual tema iremos tratar? Vou facilitar dando uma dica: Sou silenciosa, chego de mansinho... Sou filha da leviandade, da covardia... Quem me ama é inseguro e faz de mim valentia... Conto com o silêncio de todos para minha garantia... Decifra-me, ou te devoro!"
+        textoBotao.innerHTML = "Revelar"
+        }
+        respostaVisivel = !respostaVisivel
 }
 
-apertouRevelar.addEventListener('click', revelarCharada)
+
+botaoCharada.addEventListener('click', revelarCharada)
